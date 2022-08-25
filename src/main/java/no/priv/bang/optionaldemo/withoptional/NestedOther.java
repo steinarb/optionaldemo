3d51f0316809fe632ce6234d5454975b7ac2fe3a
Steinar Bang <sb@dod.no>
2022-08-25 19:36:42 +0200

    Add example of only incrementing leaf node value when the value is present

--- src/main/java/no/priv/bang/optionaldemo/withoptional/NestedOther.java
+++ src/main/java/no/priv/bang/optionaldemo/withoptional/NestedOther.java
@@ -1,11 +1,18 @@
 package no.priv.bang.optionaldemo.withoptional;
 
+import java.util.Optional;
+
 public class NestedOther {
 
     private Integer leafnodeValue;
 
     public Integer someValue() {
         return leafnodeValue;
+    }
+
+    public void incrementSomeValue() {
+        Optional.ofNullable(leafnodeValue)
+            .ifPresent(leafnode -> this.leafnodeValue = ++leafnode);
     }
 
     public NestedOther withLeafnodeValue(Integer leafnodeValue) {
--- src/test/java/no/priv/bang/optionaldemo/withoptional/WithOptionalAndFlatmapChainingWithMethodReferencesTest.java
+++ src/test/java/no/priv/bang/optionaldemo/withoptional/WithOptionalAndFlatmapChainingWithMethodReferencesTest.java
@@ -1,5 +1,6 @@
 package no.priv.bang.optionaldemo.withoptional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -59,6 +60,42 @@ class WithOptionalAndFlatmapChainingWithMethodReferencesTest {
         SomeService someService = mock(SomeService.class);
         var demo = new WithOptionalAndFlatmapChainingWithMethodReferences(someService);
 
+        assertNull(demo.findLeafnodeValue());
+    }
+
+    @Test
+    void testSuccessfullyIncrementLeafnodeValue() {
+        Integer leafnodeValue = 42;
+        SomeService someService = mock(SomeService.class);
+        when(someService.findSomething()).thenReturn(
+            Optional.ofNullable(
+                new Something().withOther(
+                    new Other().withNestedOther(
+                        new NestedOther().withLeafnodeValue(leafnodeValue)))));
+        var demo = new WithOptionalAndFlatmapChainingWithMethodReferences(someService);
+
+        assertEquals(leafnodeValue, demo.findLeafnodeValue());
+        someService.findSomething()
+            .flatMap(Something::findOther)
+            .flatMap(Other::findNestedOther)
+            .ifPresent(NestedOther::incrementSomeValue);
+        assertThat(demo.findLeafnodeValue()).isGreaterThan(leafnodeValue);
+    }
+
+    @Test
+    void testIncrementLeafnodeValueWhenValueNotSet() {
+        SomeService someService = mock(SomeService.class);
+        when(someService.findSomething()).thenReturn(
+            Optional.ofNullable(
+                new Something().withOther(
+                    new Other().withNestedOther(new NestedOther()))));
+        var demo = new WithOptionalAndFlatmapChainingWithMethodReferences(someService);
+
+        assertNull(demo.findLeafnodeValue());
+        someService.findSomething()
+            .flatMap(Something::findOther)
+            .flatMap(Other::findNestedOther)
+            .ifPresent(NestedOther::incrementSomeValue);
         assertNull(demo.findLeafnodeValue());
     }
 
